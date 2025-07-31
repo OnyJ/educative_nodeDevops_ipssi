7b7ec6d5697d493e58d48ae310931e1b83025bae
Josselin J. <44381592+OnyJ@users.noreply.github.com>
2025-07-31 15:37:21 +0200

    Feat: Create contact route (#1)
    * Edit index.js: create contact route
    
    * Useless edit index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Mon app fonctionne")
+const app = require("express")();
+
+app.get("/contact", (req, res) => res.json({ email: "toto@top.com" }));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>
+  console.log(`app listening on http://localhost:${port}`)
+);
+
+
+
